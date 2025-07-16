a61407c345fb522408d99ef4587bfbcad92da2a6
HabibPA100 <pahabib50@gmail.com>
2025-07-16 09:48:57 +0600

    pdf path

--- config/snappy.php
+++ config/snappy.php
@@ -6,7 +6,7 @@ return [
         'enabled' => true,
         'binary' => PHP_OS_FAMILY === 'Windows'
             ? env('WKHTML_PDF_BINARY', '"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe"')
-            : env('WKHTML_PDF_BINARY', base_path('"storage/app/bin/wkhtmltopdf"')),
+            : env('WKHTML_PDF_BINARY', base_path('storage/app/bin/wkhtmltopdf')),
         'timeout' => false,
         'options' => ['enable-local-file-access' => true],
         'env' => [],
@@ -16,7 +16,7 @@ return [
         'enabled' => true,
         'binary' => PHP_OS_FAMILY === 'Windows'
             ? env('WKHTML_IMG_BINARY', '"C:\Program Files\wkhtmltopdf\bin\wkhtmltoimage.exe"')
-            : env('WKHTML_IMG_BINARY', base_path('"storage/app/bin/wkhtmltoimage"')),
+            : env('WKHTML_IMG_BINARY', base_path('storage/app/bin/wkhtmltoimage')),
         'timeout' => false,
         'options' => ['enable-local-file-access' => true],
         'env' => [],
