926a584dfdce855e8aaecf0c24953ac5050dee18
HabibPA100 <pahabib50@gmail.com>
2025-07-29 19:29:42 +0600

    Pdf problem not solved

--- config/snappy.php
+++ config/snappy.php
@@ -6,7 +6,7 @@ return [
         'enabled' => true,
         'binary' => PHP_OS_FAMILY === 'Windows'
             ? env('WKHTML_PDF_BINARY', '"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe"')
-            : env('WKHTML_PDF_BINARY', base_path('storage/app/bin/wkhtmltopdf')),
+            : env('WKHTML_PDF_BINARY', base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64')),
         'timeout' => false,
         'options' => ['enable-local-file-access' => true],
         'env' => [],
@@ -16,7 +16,7 @@ return [
         'enabled' => true,
         'binary' => PHP_OS_FAMILY === 'Windows'
             ? env('WKHTML_IMG_BINARY', '"C:\Program Files\wkhtmltopdf\bin\wkhtmltoimage.exe"')
-            : env('WKHTML_IMG_BINARY', base_path('storage/app/bin/wkhtmltoimage')),
+            : env('WKHTML_IMG_BINARY', base_path('vendor/h4cc/wkhtmltoimage-amd64/bin/wkhtmltoimage-amd64')),
         'timeout' => false,
         'options' => ['enable-local-file-access' => true],
         'env' => [],
